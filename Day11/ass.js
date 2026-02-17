d5cc93fb0ece94804b09d2c2a11215353cde15d1
tanisha <tanishasingh2401@gmail.com>
2026-02-17 13:33:36 +0530

    Day 11 Completed

---
+++ Day11/ass.js
@@ -0,0 +1,28 @@
+// 1. Declare your name and print its length
+let fullName = "Tanisha Singh";
+console.log("Full Name:", fullName);
+console.log("Length of full name:", fullName.length);
+
+console.log("--------------------");
+
+// 2. Declare your first name and print its first character
+let firstName = "Tanisha";
+console.log("First Name:", firstName);
+console.log("First character:", firstName[0]);
+
+console.log("--------------------");
+
+// 3. Print last character of first name
+console.log("Last character:", firstName[firstName.length - 1]);
+
+console.log("--------------------");
+
+// 4. Output of string + number
+let result = "apnacollege" + 123;
+console.log("Concatenation Result:", result);
+
+console.log("--------------------");
+
+// 5. Length of empty string and single space string
+console.log("Length of empty string:", "".length);
+console.log("Length of single space string:", " ".length);
---
+++ Day11/practice_q1.js
@@ -0,0 +1,12 @@
+let num = 5;
+let newNum = num++;   // first value assign hoga, phir increase hoga
+
+// ab yaha:
+// num = 6
+// newNum = 5
+
+newNum = ++num;       // pehle increase hoga, phir assign hoga
+
+// final values:
+console.log(num);     // 7
+console.log(newNum);  // 7
