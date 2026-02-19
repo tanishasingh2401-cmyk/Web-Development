30ed43cfca01929360f19d17d35f8a00680f4799
tanisha <tanishasingh2401@gmail.com>
2026-02-19 12:38:21 +0530

    Day 13 Completed

---
+++ Day13/assisment_js.js
@@ -0,0 +1,77 @@
+// Ans 1
+let num = 20;
+
+if (num % 10 == 0) {
+    console.log("good");
+} else {
+    console.log("bad");
+}
+
+
+// Ans 2
+let name = prompt("Please enter your name:");
+let age = prompt("Please enter your age:");
+
+alert(`${name} is ${age} years old.`);
+
+
+// Ans 3
+let quarter = 1;
+
+switch (quarter) {
+    case 1:
+        console.log("January, February, March");
+        break;
+    case 2:
+        console.log("April, May, June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
+    default:
+        console.log("NOT A QUARTER!");
+}
+
+
+// Ans 4
+let str = "apples";
+
+if ((str[0] == 'a' || str[0] == 'A') && (str.length > 5)) {
+    console.log("golden string");
+} else {
+    console.log("not a golden string");
+}
+
+
+// Ans 5
+let a = 5;
+let b = 18;
+let c = 13;
+
+if (a > b) {
+    if (a > c) {
+        console.log(a + " is largest");
+    } else {
+        console.log(c + " is largest");
+    }
+} else {
+    if (b > c) {
+        console.log(b + " is largest");
+    } else {
+        console.log(c + " is largest");
+    }
+}
+
+
+// Ans 6 (Bonus)
+let num1 = 32;
+let num2 = 47852;
+
+if (num1 % 10 == num2 % 10) {
+    console.log("Numbers have the same last digit which is " + (num1 % 10));
+} else {
+    console.log("Numbers don't have the same last digit");
+}
