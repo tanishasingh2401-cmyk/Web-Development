ba428e3a23e7f3043e35088a954a9d228b856ab4
tanisha <tanishasingh2401@gmail.com>
2026-03-23 23:28:06 +0530

    Day 18 Completed

--- Day18/DOM(webpage)/app.js
+++ Day18/DOM(webpage)/app.js
@@ -1,2 +1,19 @@
 //JAVASCRIPT PART-9
-//web-page
+
+//1st
+//let para1=document.createElement("p");
+//para1.ineerText="Hey I'm Red!";
+//document.querySelector("body").append(para1);
+
+//para1.classList.add("red");
+
+
+
+
+//2nd
+
+//let h3 = document.createElement("h3");
+//h3.ineerText=" I'm a blue h3!";
+//document.querySelector("body").append(h3);
+
+//para1.classList.add("blue");
