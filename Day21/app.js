f0ae1e072c87f50b9432d1762befe0d7d306434d
tanisha <tanishasingh2401@gmail.com>
2026-03-28 22:18:30 +0530

    Day 22 Completed

---
+++ Day21/app.js
@@ -0,0 +1,18 @@
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let lis = document.querySelectorAll("li");
+
+div.addEventListener("click",function () {
+    console.log("div was clicked");
+});
+
+ul.addEventListener("click", function () {
+  console.log("ul was clicked");
+});
+
+for (li of lis) {
+    li.addEventListener("clicked", function () {
+        console.log("li was clicked");
+    });
+}
+
---
+++ Day22/app.js
@@ -0,0 +1,39 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    console.log(inp.value);
+    item.innerText = inp.value;
+
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+
+
+    item.appendChild(delbtn);
+
+
+
+    ul.appendChild(item);
+    inp.value="";
+    
+});
+
+
+let delBtns =  document.querySelectorAll(".delete");
+for(delBtn of delBtns) {
+    delBtn.addEventListener("click", function () {
+        let par = this.parentElement;
+        console.log(par);
+        par.remove();
+
+        
+
+
+    });
+
+}
+
